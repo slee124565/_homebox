84bc9ced75bc9ab1c8a0b7d0460119e4d2538612
Lee Shiueh <lee.shiueh@gmail.com>
2016-12-19 10:28:58 +0800

    add yo angular initial files

---
+++ webapp/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name homeboxApp
+ * @description
+ * # homeboxApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('homeboxApp', [
+    'ngResource'
+  ]);
