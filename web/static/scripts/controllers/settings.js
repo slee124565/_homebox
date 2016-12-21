086e2a44395deff62baeefb29a1f0d84bb92c267
Lee Shiueh <lee.shiueh@gmail.com>
2016-12-21 14:02:19 +0800

    fix

--- web/static/scripts/controllers/settings.js
+++ web/static/scripts/controllers/settings.js
@@ -32,6 +32,7 @@ angular.module('homeboxApp')
                     wifiPassword: wifiConfig.psk
                 };
                 $scope.ssidOptions = config.ssidOptions;
+                console.log($scope.siteConfig.ssidSelected);
             },function(response){
                 $scope.errMessage = 'Fail to get site config data!';
                 console.log($scope.errMessage);
