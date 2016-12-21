2f8f3a09c4baa7a82f6b707388f523cdd9eae648
Lee Shiueh <lee.shiueh@gmail.com>
2016-12-21 11:51:03 +0800

    revised

--- web/ngapp/app/scripts/controllers/settings.js
+++ web/ngapp/app/scripts/controllers/settings.js
@@ -2,8 +2,8 @@
 
 angular.module('homeboxApp')
     .controller('SettingsCtrl', 
-    ['$http','$scope',
-    function($http, $scope) {
+    ['$http','$scope','SettingsService',
+    function($http, $scope, SettingsService) {
         $scope.title = 'FLH Homebox';
         $scope.description = 'Make your Fibaro HC2/HCL connected with Homekit APP';
         $scope.header = 'System Configurations';
@@ -17,32 +17,29 @@ angular.module('homeboxApp')
         };
         $scope.ssidOptions = $scope.siteConfig.ssidOptions;
         $scope.wifiSSID = $scope.siteConfig.wifiSSID;
-        $scope.getSiteConfig();
         
-        $scope.getSiteConfig = function() {
-            $http({
-                method: 'GET',
-                url: '/webapp/api/config'
-            }).then(
-                function(response) {
-                    $scope.config = JSON.parse(response.data);
-                    $scope.siteConfig = {
-                        hc2IPAddress: config.hc2.hc2_hostname,
-                        hc2Account: config.hc2.hc2_account,
-                        hc2Password: config.hc2.hc2_password,
-                        ssidOptions: config.ssidOptions,
-                        wifiSSID: {name: config.wifi.ssid},
-                        wifiPass: config.wifi.psk
-                    };
-                    $scope.ssidOptions = $scope.siteConfig.ssidOptions;
-                    $scope.wifiSSID = $scope.siteConfig.wifiSSID;
-                    $scope.wifiPassword = $scope.siteConfig.wifiPass;
-                },function(response){
-                    $scope.errMessage = 'Fail to get site config data!';
-                    console.log($scope.errMessage);
-                    console.log(response.statusText);
-                });
-        };
+        SettingsService.getSiteConfig().then(
+            function(response) {
+                var config = JSON.parse(response.data);
+                var hc2Config = config.hc2;
+                var wifiConfig = config.wifi;
+                $scope.siteConfig = {
+                    hc2IPAddress: hc2Config.hc2_hostname,
+                    hc2Account: hc2Config.hc2_account,
+                    hc2Password: hc2Config.hc2_password,
+                    ssidOptions: config.ssidOptions,
+                    wifiSSID: {name: wifiConfig.ssid},
+                    wifiPass: wifiConfig.psk
+                };
+                $scope.ssidOptions = config.ssidOptions;
+                $scope.wifiSSID = $scope.siteConfig.wifiSSID;
+                $scope.wifiPassword = $scope.siteConfig.wifiPass;
+            },function(response){
+                $scope.errMessage = 'Fail to get site config data!';
+                console.log($scope.errMessage);
+                console.log(response.statusText);
+            });
+
         $scope.setSiteConfig = function() {
             $http({
                 method: 'POST',
