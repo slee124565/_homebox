f7b38582feec8f23b778a686e3f094c0ba4c3643
Lee Shiueh <lee.shiueh@gmail.com>
2016-12-22 14:16:38 +0800

    add system update script

---
+++ utils/sys_update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+proj_list="/usr/share/homebox"
+
+for proj in ${proj_list}
+do
+    if [ -d ${proj} ]; then
+        cd ${proj}
+        git checkout -f && git clean -fx -d && git checkout master && git pull origin master
+        cd -
+    fi
+done
+
+/sbin/shutdown -r 3
+
